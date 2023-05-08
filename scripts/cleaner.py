597c28c9649489540bca4eec135fc675d00f6baf
Emmet Halm <69861956+emmethalm@users.noreply.github.com>
2023-05-08 15:22:02 +0900

    Update cleaner.py
    You used to code it in Python
    Late-night when you need that speed
    Type it out in Python
    Late-night when you need that speed
    
    And I know when that script starts threading
    That can only mean one thing
    I know when that script starts threading
    That can only mean one thing
    
    Using tokens, oh, unlimited
    Call the OpenAI API
    Working faster, cleaning transcripts
    As the late-night hours go by
    
    And I know when that code starts syncing
    That can only mean one thing
    I know when that code starts syncing
    That can only mean one thing

--- scripts/cleaner.py
+++ scripts/cleaner.py
@@ -1,6 +1,7 @@
 import openai
+from concurrent.futures import ThreadPoolExecutor
 
-openai.api_key = "sk-XXXXXXXXXXXXXXXXXX"
+openai.api_key = "sk-XXXXXXXXXXXXXXXXXXXXXXXXXX"
 
 def load_text(file_path):
     with open(file_path, 'r') as file:
@@ -15,18 +16,17 @@ def call_openai_api(chunk):
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": "You are an assistant that cleans transcripts for grammatical mistakes, punctuation, and filler words. You are given a transcript between figures. You must edit the transcript for grammatical correctness, add correct punctuation, identify the speakers, remove filler words like 'um' and 'like', and return it to the user. Use context to help you identify mistranscribed words, punctuation, and who is speaking."},
+            {"role": "system", "content": "You are an assistant that cleans transcripts for grammatical mistakes, punctuation, and filler words. You are given a transcript between an interviewer and their guest. You must edit the transcript for grammatical correctness, add correct punctuation, identify the speakers, remove filler words like 'um' and 'like', and return it to the user. Use context to help you identify mistranscribed words, punctuation, and who is speaking."},
             {"role": "user", "content": f"Please clean this transcript: {chunk}."},
         ],
-        max_tokens=4000,
+        max_tokens=1750,
         n=1,
         stop=None,
         temperature=0.5,
     )
     return response.choices[0]['message']['content'].strip()
 
-
-def split_into_chunks(text, tokens=3000):
+def split_into_chunks(text, tokens=1500):
     words = text.split()
     chunks = [' '.join(words[i:i + tokens]) for i in range(0, len(words), tokens)]
     return chunks
@@ -34,7 +34,11 @@ def split_into_chunks(text, tokens=3000):
 def process_chunks(input_file, output_file):
     text = load_text(input_file)
     chunks = split_into_chunks(text)
-    responses = [call_openai_api(chunk) for chunk in chunks]
+    
+    # Use ThreadPoolExecutor to process chunks in parallel
+    with ThreadPoolExecutor() as executor:
+        responses = list(executor.map(call_openai_api, chunks))
+
     save_to_file(responses, output_file)
 
 if __name__ == "__main__":
