c45969c72c10b823faf5444fab84b26d481196e1
Emmet Halm <69861956+emmethalm@users.noreply.github.com>
2023-05-08 15:17:11 +0900

    Update prompt_completion_gen.py
    Script too smart, don't code too close
    You gon' level up and rise on this wave
    Crushing all these bugs, I've been in the zone
    I don't care where I go, long as code's well-made
    
    Clean lil' line, it's been on my mind
    Soon as I refactor, it's getting displayed
    Do this all the time, this ain't no surprise
    Every other night, a new function gets made
    
    Script too tight, don't doubt this prose
    You gon' find your way and grow in this trade
    Optimizing loops, I've been on the grind
    I don't care where I start, long as progress is made

--- scripts/prompt_completion_gen.py
+++ scripts/prompt_completion_gen.py
@@ -1,6 +1,7 @@
 import openai
+from concurrent.futures import ThreadPoolExecutor
 
-openai.api_key = "sk- "sk-XXXXXXXXXXXXXXXXXX"
+openai.api_key = "sk-XXXXXXXXXXXXXXXXXX"
 
 def load_text(file_path):
     with open(file_path, 'r') as file:
@@ -15,18 +16,20 @@ def call_openai_api(chunk):
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": "I have the following text transcript that in need to put in JSONL format for fune tuning. Example format, "{"prompt": "Who is Elon Musk?", "completion": "Elon Musk is a business magnate and investor."}" Please generate 10 to 15 appropriate questions for the following text and place the questions and text in JSONL format. The 'completion' should come verbatim from the text. You must use the 'you' form for questions. You must utilize the entire text:"},
-            {"role": "user", "content": f"Please clean this transcript: {chunk}."},
+            {
+                "role": "system",
+                "content": 'I have the following text transcript that in need to put in JSONL prompt-completion pair format for fune tuning. Example format, "{\\"prompt\\": \\"Who are you?\\", \\"completion\\": \\"I am Lebron James, an American professional basketball player for the Los Angeles Lakers and an all around nice guy.\\"}" Please generate 10 to 15 appropriate questions for the following text and place the questions and text in JSONL format. The \'completion\' should come verbatim from the text. You must use the \'you\' form for all prompt questions, never say "the speaker". You must utilize the entire text in the completion. Always answer the completion from the first person:',
+            },
+            {"role": "user", "content": f"Transcript: {chunk}."},
         ],
-        max_tokens=4000,
+        max_tokens=1750,
         n=1,
         stop=None,
         temperature=0.5,
     )
     return response.choices[0]['message']['content'].strip()
 
-
-def split_into_chunks(text, tokens=3000):
+def split_into_chunks(text, tokens=1500):
     words = text.split()
     chunks = [' '.join(words[i:i + tokens]) for i in range(0, len(words), tokens)]
     return chunks
@@ -34,7 +37,11 @@ def split_into_chunks(text, tokens=3000):
 def process_chunks(input_file, output_file):
     text = load_text(input_file)
     chunks = split_into_chunks(text)
-    responses = [call_openai_api(chunk) for chunk in chunks]
+    
+    # Use ThreadPoolExecutor to process chunks in parallel
+    with ThreadPoolExecutor() as executor:
+        responses = list(executor.map(call_openai_api, chunks))
+
     save_to_file(responses, output_file)
 
 if __name__ == "__main__":
